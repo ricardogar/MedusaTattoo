9cbd8efa0fce95758c5d093352b04c13e04ac974
Julian David <caprijuda@gmail.com>
2018-06-23 16:19:52 -0500

    added new icons & logos

--- src/main/webapp/app/layouts/navbar/navbar.controller.js
+++ src/main/webapp/app/layouts/navbar/navbar.controller.js
@@ -12,7 +12,6 @@
 
         vm.isNavbarCollapsed = true;
         vm.isAuthenticated = Principal.isAuthenticated;
-
         ProfileService.getProfileInfo().then(function(response) {
             vm.inProduction = response.inProduction;
             vm.swaggerEnabled = response.swaggerEnabled;
